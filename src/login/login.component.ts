6661cf5aa919d5db78c1acc6c63b2aab57ca1798
ilija-malkier <ilija.radojkovic@malkier.rs>
2023-08-01 10:56:24 +0200

    Added login page

---
+++ src/login/login.component.ts
@@ -0,0 +1,20 @@
+import {Component, ElementRef, OnInit} from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {AuthService} from "../services/auth.service";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent{
+
+
+  constructor(private auth:AuthService) {
+  }
+
+
+  login(form: NgForm) {
+
+  }
+}
