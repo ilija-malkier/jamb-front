7f1a42ed322d31f78243402d14215cf5186ef4cb
ilijaradojkovic <radojkovicika@gmail.com>
2023-08-31 11:43:36 +0200

    Added registration-modal.component.ts

---
+++ src/app/registration-modal/registration-modal.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ModalService} from "../../services/modal.service";
+
+@Component({
+  selector: 'app-registration-modal',
+  templateUrl: './registration-modal.component.html',
+  styleUrls: ['./registration-modal.component.css']
+})
+export class RegistrationModalComponent implements OnInit,OnDestroy{
+
+  registerModalId="register-modal"
+  public static  registerModalId="register-modal"
+  registerModalTitle="Registration"
+  constructor(private modalService:ModalService) {}
+
+  ngOnInit(): void {
+    this.modalService.register(this.registerModalId)
+  }
+
+  ngOnDestroy(): void {
+    this.modalService.unregister(this.registerModalId)
+
+  }
+
+  closeModal() {
+    this.modalService.closeModal(this.registerModalId)
+  }
+}
